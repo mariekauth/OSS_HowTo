6eeb58974f6e7b609e586835bc204e2eb95d730e
Marie Kauth <marie.kauth@gmail.com>
2016-07-19 11:22:34 -0400

    Add Home Link
    This is a test

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php echo 'Hello World!'; ?>
 <ul>
+    <li><a href="/">Home</a></li>
     <li><a href="https://git-scm.com/book" target="_blank">Git</a></li>
     <li><a href="http://php-fig.com" target="_blank">PHP Standards</a></li>
     <li><a href="https://gitter.im/brianfreytag/git-chat" target="_blank">Gitter</a></li>
